663351f5c2a5f692e31da0df00e465cd15c4dea4
Kye_Cook <acfanatics@hotmail.com>
2016-08-22 17:27:17 +1000

    Fixed majority of code

--- src/Part2/CountByThrees.java
+++ src/Part2/CountByThrees.java
@@ -4,12 +4,16 @@ package Part2;
  * Created by Kye_Cook on 22/08/2016.
  */
 public class CountByThrees {
-    public static void main(String[] args)
-    {
-        int num = 3;
+    public static void main(String[] args) {
 
-        while (num < 300)
-            num += 3;
-            System.out.println(num + ",");
+//        while (num < 300)
+//            System.out.println(num+=3);
+//    }
+
+        for (int num = 3; num < 300; num += 3) {
+            System.out.print(num + ", ");
+            if(num % 30 == 0)
+                    System.out.println("\n");
+        }
     }
 }
--- src/Part2/EvenEntryLoop.java
+++ src/Part2/EvenEntryLoop.java
@@ -8,6 +8,16 @@ public class EvenEntryLoop {
     public static void main(String[] args)
     {
         int userInteger = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter integer"));
-        
+
+        while(userInteger != 999)
+            if(userInteger % 2 == 0) {
+                JOptionPane.showMessageDialog(null, "Good Job!");
+                userInteger = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter integer"));
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Error! Try again");
+                userInteger = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter integer"));
+            }
+        JOptionPane.showMessageDialog(null, "Sentinel Value invoked, loop ended");
     }
 }
